b24b23ec6804c565e8fbbd04342f423a2ccad651
ntk <43851479+ntk-ta01@users.noreply.github.com>
2021-08-07 18:38:48 +0900

    全マスdfsのうち、突き進んでも意味ないところを排除 	7187907点

--- src/main.rs
+++ src/main.rs
@@ -29,12 +29,29 @@ fn main() {
     let input = Input { n, s, c };
     let mut route = vec![];
     let mut visited = vec![vec![false; n]; n];
-    dfs(&input, input.s, &mut visited, &mut route);
+    let mut seen = vec![vec![false; n]; n];
+    dfs(&input, input.s, &mut visited, &mut route, &mut seen);
     let answer = route.iter().map(|idx| DIR[*idx]).collect::<String>();
     println!("{}", answer);
 }
 
-fn dfs(input: &Input, v: (usize, usize), visited: &mut [Vec<bool>], route: &mut Vec<usize>) {
+fn dfs(
+    input: &Input,
+    v: (usize, usize),
+    visited: &mut [Vec<bool>],
+    route: &mut Vec<usize>,
+    seen: &mut [Vec<bool>],
+) {
+    // 頂点vから視界に入るところを全部trueに
+    for (di, dj) in DIJ.iter() {
+        let mut next_i = v.0 + *di;
+        let mut next_j = v.1 + *dj;
+        while next_i < input.n && next_j < input.n && input.c[next_i][next_j] != '#' {
+            seen[next_i][next_j] = true;
+            next_i += *di;
+            next_j += *dj;
+        }
+    }
     for (oi, (di, dj)) in DIJ.iter().enumerate() {
         let next_i = v.0 + *di;
         let next_j = v.1 + *dj;
@@ -47,7 +64,9 @@ fn dfs(input: &Input, v: (usize, usize), visited: &mut [Vec<bool>], route: &mut 
         }
         visited[next_i][next_j] = true;
         route.push(oi);
-        dfs(input, (next_i, next_j), visited, route);
+        if !is_partolled(input, next_i, next_j, (*di, *dj), seen) {
+            dfs(input, (next_i, next_j), visited, route, seen);
+        }
         let b_oi = match oi {
             0 => 3,
             1 => 2,
@@ -57,4 +76,48 @@ fn dfs(input: &Input, v: (usize, usize), visited: &mut [Vec<bool>], route: &mut 
         };
         route.push(b_oi);
     }
+}
+
+// そのまま進んだ場合に周辺の道がすべてパトロールされているかチェック
+fn is_partolled(
+    input: &Input,
+    next_i: usize,
+    next_j: usize,
+    dir: (usize, usize),
+    seen: &[Vec<bool>],
+) -> bool {
+    let mut next_i = next_i;
+    let mut next_j = next_j;
+    let (nextnext_i1, nextnext_j1, nextnext_i2, nextnext_j2) = if dir.0 == 0 {
+        // 横向きに直進 上と下を調べる
+        if next_i == 0 {
+            (0, 0, 1, 0)
+        } else if next_i == input.n - 1 {
+            (1usize.wrapping_neg(), 0, 0, 0)
+        } else {
+            (1usize.wrapping_neg(), 0, 1, 0)
+        }
+    } else {
+        // 縦向きに直進 左と右を調べる
+        if next_j == 0 {
+            (0, 0, 0, 1)
+        } else if next_j == input.n - 1 {
+            (0, 1usize.wrapping_neg(), 0, 0)
+        } else {
+            (0, 1usize.wrapping_neg(), 0, 1)
+        }
+    };
+    while next_i < input.n && next_j < input.n && input.c[next_i][next_j] != '#' {
+        // next_i, next_jの周辺が全てseen || '#'ならOK
+        if !(seen[next_i + nextnext_i1][next_j + nextnext_j1]
+            || input.c[next_i + nextnext_i1][next_j + nextnext_j1] == '#')
+            || !(seen[next_i + nextnext_i2][next_j + nextnext_j2]
+                || input.c[next_i + nextnext_i2][next_j + nextnext_j2] == '#')
+        {
+            return false;
+        }
+        next_i += dir.0;
+        next_j += dir.1;
+    }
+    true
 }
