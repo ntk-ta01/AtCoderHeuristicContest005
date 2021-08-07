f15dd8338ee441070449208189bb02d17f13afcf
ntk <43851479+ntk-ta01@users.noreply.github.com>
2021-08-07 16:20:39 +0900

    first submit 空行 19878点

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+#![allow(non_snake_case, dead_code, unused_imports, unused_macros)]
+
+use proconio::{input, marker::*, source::Source};
+
+const DIJ: [(usize, usize); 4] = [
+    (0, 1usize.wrapping_neg()),
+    (1, 0),
+    (0, 1),
+    (1usize.wrapping_neg(), 0),
+];
+const DIR: [char; 4] = ['U', 'L', 'R', 'D'];
+
+type Output = String;
+
+struct Input {
+    n: usize,
+    s: (usize, usize),
+    c: Vec<Vec<char>>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    input! {
+        n: usize,
+        _: (usize, usize),
+        _: [Chars; n],
+    }
+    println!();
 }
